0ca2a4c7ca977e1c26606a34e7541dbb37508031
joao <jotah@gmail.com>
2024-10-03 18:59:32 -0300

    primeiro commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int nota1 = 11;
+        int nota2 = 22;
+        int nota3 = 33;
+        int nota4 = 44;
+
+        int soma = nota1 + nota2 + nota3 + nota4;
+
+        int media = soma / 4;
+        String documento = ("olá Professor!!! a soma entre as 4 notas é "+soma+" e a media é igual a "+media);
+        System.out.println(documento);
+    }
+
+
+}
